f542934cb4afd218c52a02aa2814a9252da95ea3
Daniel <dandesantis14@gmail.com>
2021-12-19 21:10:35 -0500

    commit tree b46798b553b4b55c6c15b14c70552bc69e1425f8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,21 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      if user
+        items = user.items
+      else
+        render json: {errors: "not found"}, status: :not_found
+      end
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def create
+
   end
 
 end
